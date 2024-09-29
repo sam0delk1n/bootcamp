a37bcca36417d727abdc44a79b90db081ba21aa8
sam0delk1n <sam0delk1n@yandex.ru>
2024-09-30 00:06:06 +0300

    Небольшие исправления в Практике 12.13

--- module-12/ex13/Program.cs
+++ module-12/ex13/Program.cs
@@ -1,19 +1,19 @@
 ﻿Console.Write("Количество человек (1): ");
-uint units = uint.TryParse( Console.ReadLine(), out units ) ? units : 1;
+uint units = UInt32.TryParse( Console.ReadLine(), out units ) ? units : 1;
 if (units < 1) {
     Console.WriteLine("FATAL ERROR");
     return;
 }
 
 Console.Write("Сумма чека (1000): ");
-double amount = double.TryParse( Console.ReadLine(), out amount ) ? amount : 1000;
+double amount = Double.TryParse( Console.ReadLine(), out amount ) ? amount : 1000;
 if (amount < 0) {
     Console.WriteLine("FATAL ERROR");
     return;
 }
 
 Console.Write("Процент чаевых (25): ");
-double tips = double.TryParse( Console.ReadLine(), out tips ) ? tips : 25;
+double tips = Double.TryParse( Console.ReadLine(), out tips ) ? tips : 25;
 if (tips < 0) {
     Console.WriteLine("FATAL ERROR");
     return;
