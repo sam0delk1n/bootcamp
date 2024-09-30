28ee2409b193ff27d747a420f9c340d48007ceef
sam0delk1n <sam0delk1n@yandex.ru>
2024-09-30 03:21:57 +0300

    Практика 12.19

---
+++ module-12/ex19/Program.cs
@@ -0,0 +1,15 @@
+﻿const string correct = "Всё верно!";
+const string wrong   = "Нужно подумать ещё...";
+
+uint score = 0;
+
+Console.InputEncoding  = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+Console.Write( "Что означает слово 'cat'? [кошка, собака]: " );
+Console.WriteLine( ( Console.ReadLine() ?? "null" ) == "кошка" ? (score += 10, correct) : wrong );
+
+Console.Write( "Что означает слово 'dog'? [кошка, собака]: " );
+Console.WriteLine( ( Console.ReadLine() ?? "null" ) == "собака" ? (score += 10, correct) : wrong );
+
+Console.WriteLine( $"Счёт: {score}" );
