697cf9af962f7c73725d192194e88084c41cf5c6
sam0delk1n <sam0delk1n@yandex.ru>
2024-09-30 22:48:25 +0300

    Практика 12.22

---
+++ module-12/ex22/Program.cs
@@ -0,0 +1,34 @@
+﻿const string correct = "Всё верно!";
+const string wrong   = "Нужно подумать ещё...";
+
+uint   score = 0;
+string answer;
+
+Console.InputEncoding  = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+Console.Write( "Что означает слово 'cat'? [кошка, собака]: " );
+
+answer = Console.ReadLine() ?? "null";
+answer = answer.Trim().ToLower();
+
+if (answer == "кошка") {
+    score += 10;
+    Console.WriteLine(correct);
+} else {
+    Console.WriteLine(wrong);
+}
+
+Console.Write( "Что означает слово 'dog'? [кошка, собака]: " );
+
+answer = Console.ReadLine() ?? "null";
+answer = answer.Trim().ToLower();
+
+if (answer == "собака") {
+    score += 10;
+    Console.WriteLine(correct);
+} else {
+    Console.WriteLine(wrong);
+}
+
+Console.WriteLine( $"Счёт: {score}" );
